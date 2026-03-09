518c6b1fd5734412b97d7db46474899a747943e3
Heng David <hengdavid129@gmail.com>
2026-03-09 09:31:49 +0700

    Build a Better Router

--- Core/functions.php
+++ Core/functions.php
@@ -16,19 +16,31 @@ function urlIs($value)
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
-function authorize($condition, $status = Response::FORBIDDEN) {
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require base_path("views/{$code}.php");
+
+    die();
+}
+
+function authorize($condition, $status = Response::FORBIDDEN)
+{
     if (! $condition) {
         abort($status);
     }
 }
 
-function base_path($path) {
+function base_path($path)
+{
     return BASE_PATH . $path;
 }
 
-function view($path, $attribute = []) {
-    
-   extract($attribute);
+function view($path, $attribute = [])
+{
+
+    extract($attribute);
 
     require base_path('views/' . $path);
 }
