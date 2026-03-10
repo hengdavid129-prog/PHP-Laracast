fe468771691d2ef36c5f9e93267675ca5d50beda
Heng David <hengdavid129@gmail.com>
2026-03-10 09:39:53 +0700

    Register a New User

--- Core/Validator.php
+++ Core/Validator.php
@@ -10,7 +10,7 @@ class Validator
         return strlen($value) >= $min && strlen($value) <= $max;
     }
 
-    // public static function email($value) {
-    //     return filter_var($value, FILTER_VALIDATE_EMAIL);
-    // }
+    public static function email($value) {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
 }
